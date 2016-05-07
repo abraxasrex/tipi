c5824f7b0534a078fd28dfb2f3f3b6534400336a
abraxasrex <jbreasor@gmail.com>
2016-05-06 19:18:11 -0700

    removed regex dep

--- server.js
+++ server.js
@@ -13,11 +13,13 @@ app.engine('tipi', function(filepath, options, callback ){
   });
 });
 
+
+require('tipi.js')(app);
 app.set('views', './views');
 app.set('view engine', 'tipi');
 
 app.get('/', function (req, res) {
-  res.render('test', { sampleH: 'Hello world!', sampleP: 'this is a paragraph'});
+  res.render('example', { sampleH: 'Hello world!', sampleP: 'this is a paragraph'});
 });
 
 var port = process.env.PORT || 8080;
---
+++ tipi.js
@@ -0,0 +1,15 @@
+
+module.exports = function(app){
+  //define engine
+  app.engine('tipi', function(filepath, options, callback ){
+    fs.readFile(filepath, function(err, content){
+      if(err){
+        return callback(new Error(err));
+      }
+      var rendered = content.toString().replace(/\^a\^/g, '<h1>' + options.sampleH + '</h1>')
+        .replace(/\^b\^/g, '<p>' + options.sampleP + '</p>');
+      return callback(null, rendered);
+    });
+  });
+
+}
